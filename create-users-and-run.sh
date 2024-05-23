de1955c1f1ca2d539aaceda20389c8de41f2fab9
Vincenzo Ingrosso <expert@slackarea.net>
2024-05-23 13:04:14 +0200

    script correct

--- create-users-and-run.sh
+++ create-users-and-run.sh
@@ -9,6 +9,5 @@ echo $USER:$(head -c30 /dev/urandom | base64) | chpasswd
 mkdir -p /home/$USER/.ssh
 chown -R $USER:rsync /home/$USER/.ssh
 chmod -R go-wx /home/$USER/.ssh
-done
 
 exec /usr/sbin/sshd -eD
